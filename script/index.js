6f7bad57906f6e9d25581a54fe438324c95e0d3b
mystique09 <knightkrypto7@gmail.com>
2021-04-20 23:28:23 +0800

    fixed mobile responsiveness issue

---
+++ script/index.js
@@ -0,0 +1,23 @@
+function translate(sentence) {
+  const rev = sentence.split(' ');
+  const capAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+  if (rev.length >= 2) {
+    return rev.map((word, index)=> {
+      word = word.split('');
+      let isCapital = capAlphabet.includes(word[0]);
+      word.push(word.shift());
+      word[0] = isCapital & word[0] .toLocaleUpperCase();
+      word[word.length-1] = word[word.length - 1].toLocaleLowerCase();
+      return word.join('') + 'ay';
+    }).join(' ');
+  } else {
+    let word = rev[0].split(''),
+    isCapital = capAlphabet.includes(word[0]);
+    word.push(word.shift());
+    word[0] = isCapital & word[0].toLocaleUpperCase();
+    return word.join('') + 'ay';
+  }
+}
+
+console.log(translate('Hello World'))
